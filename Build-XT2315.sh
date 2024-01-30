82bd2dfa0d7d1fd6325260d132a13c8802a086ee
PizzaG <pizzag98@gmail.com>
2024-01-30 03:57:59 -0600

    Initial OrangeFox Commit

--- Build-XT2315.sh
+++ Build-XT2315.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-APP_NAME="SkyHawk - XT2315" 
+APP_NAME="OrangeFox - XT2315" 
 
 # Print Rom Name To Terminal
 echo -ne "\033]0;$APP_NAME\007"
 
-## SHRP
+## OFRP
 #############################
 export USE_CCACHE=1
 . build/envsetup.sh
 m clean
 lunch twrp_yume-eng
+echo ""
+echo "Adding Magisk From Device Tree Prebuilt Folder"
+cp -r device/motorola/yume/prebuilt/Magisk.zip vendor/recovery/FoxFiles/Magisk.zip
+sleep 5
+echo ""
 mka adbd recoveryimage
-echo
+echo ""
 echo " Recovery Should Be Built"
 echo ""
-mv $OUT/recovery.img ~/Desktop/SkyHawk-12.1-Stylus_5G_2023-XT2315.img
-mv $OUT/SHRP_v*.zip ~/Desktop/SkyHawk-12.1-Stylus_5G_2023-XT2315.zip
-mv $OUT/SHRP_Addon*.zip ~/Desktop/SkyHawk-AddonRescue-Stylus_5G_2023-XT2315.zip
+mv $OUT/OrangeFox-*.img ~/Desktop/OrangeFox_12.1-Stylus_5G_2023-XT2315.img
+mv $OUT/OrangeFox-*.zip ~/Desktop/OrangeFox_12.1-Stylus_5G_2023-XT2315.zip
 m clean
 read
 #############################
